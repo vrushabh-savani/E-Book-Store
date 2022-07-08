9c48ef078f58f49ce0588f74667fa8464b93de62
Vrushabh Savani <94985247+vrushabh-savani@users.noreply.github.com>
2022-07-08 20:19:25 +0530

    Add files via upload

---
+++ BackEnd/BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Models.ViewModels;
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+       protected readonly myBookStoreContext _context = new myBookStoreContext();
+    }
+}
