4f48aad6b8bedbe791bb2b09ac05b4caded9a712
Vrushabh Savani <94985247+vrushabh-savani@users.noreply.github.com>
2022-07-08 20:15:33 +0530

    Add files via upload

---
+++ BackEnd/BookStore.Models/Models/ListResponse.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace BookStore.Models.Models
+{
+    public class ListResponse<T> where T : class
+    {
+        public IEnumerable<T> results { get; set; }
+
+        public int TotalRecords { get; set; }
+    }
+}
